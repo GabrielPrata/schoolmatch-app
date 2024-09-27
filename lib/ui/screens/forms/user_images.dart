525e2b378ea48f6d504c9fb2ab180de8abc57241
Gabriel Prata <gabrielsprata@hotmail.com>
2024-09-27 17:26:57 -0300

    [wip] Desenvolvimento da tela de upload de imagens

--- lib/ui/screens/forms/user_images.dart
+++ lib/ui/screens/forms/user_images.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:reorderable_grid_view/reorderable_grid_view.dart';
 import 'package:school_match/domain/controllers/new_user_controller.dart';
 import 'package:school_match/ui/screens/forms/user_lastname.dart';
 import 'package:school_match/ui/widgets/forms/images_picker.dart';
@@ -22,25 +21,38 @@ class _UserImagesState extends State<UserImages> {
   final ImagePicker imagePicker = ImagePicker();
   List<XFile> _imageFiles = [];
 
-  void selectImages() async {
-    try {
-      final List<XFile>? selectedImages = await imagePicker.pickMultiImage();
-      if(selectedImages!.isNotEmpty){
-        _imageFiles.addAll(selectedImages);
-      }
-      setState(() {
-        
-      });
-    } catch (e) {
-      print("erro ao selecionar a imagem");
-      print(e);
-    }
-  }
-
   @override
   void initState() {
     super.initState();
+    
+    // Adiciona 6 imagens padrão
+    for (int i = 0; i < 6; i++) {
+      _imageFiles.add(XFile("assets/emptyPhoto.png"));  // Imagem padrão
+    }
+    
     userController.step += 1;
+  }
+
+  // Função para selecionar as imagens
+  void selectImages() async {
+    try {
+      final List<XFile>? selectedImages = await imagePicker.pickMultiImage();
+      if (selectedImages != null && selectedImages.isNotEmpty) {
+        // Substitui as imagens padrão pelas novas selecionadas
+        for (var i = 0; i < selectedImages.length; i++) {
+          if (_imageFiles.any((image) => image.name == 'emptyPhoto.png')) {
+            // Substitui a primeira imagem padrão encontrada
+            final index = _imageFiles.indexWhere((image) => image.name == 'emptyPhoto.png');
+            if (index != -1) {
+              _imageFiles[index] = selectedImages[i];
+            }
+          }
+        }
+      }
+      setState(() {});
+    } catch (e) {
+      print("Erro ao selecionar a imagem: $e");
+    }
   }
 
   @override
@@ -71,10 +83,32 @@ class _UserImagesState extends State<UserImages> {
                   : Image.asset("assets/LogoSchoolMatch.png"),
             ),
             const SizedBox(height: 30),
-            ImagesPicker(imageFiles: _imageFiles,),
+            SizedBox(
+              child: Text(
+                "É hora das fotos!",
+                style: Theme.of(context).textTheme.titleMedium,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 5),
+            SizedBox(
+              child: Text(
+                "Escolha pelo menos 2 fotos.\nUma dica, deixe as melhores fotos na frente ;)",
+                style: Theme.of(context).textTheme.bodySmall,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 50),
+            ImagesPicker(
+              newImageFunction: selectImages,  // Aqui está passando a função
+              imageFiles: _imageFiles,
+              allowReorderingUserImagesOnly: true,  // Modificação importante
+            ),
+            SizedBox(
+              height: 180,
+            ),
             ElevatedButton(
-              // onPressed: enviarPrimeiroNome,
-              onPressed: selectImages,
+              onPressed: enviarPrimeiroNome,
               child: Text(
                 "PRÓXIMO",
                 style: Theme.of(context).textTheme.labelMedium,
--- lib/ui/widgets/forms/images_picker.dart
+++ lib/ui/widgets/forms/images_picker.dart
@@ -1,44 +1,114 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-
 import 'package:reorderable_grid_view/reorderable_grid_view.dart';
 
 class ImagesPicker extends StatefulWidget {
   final List<XFile> imageFiles;
+  final bool allowReorderingUserImagesOnly;  // Controle para reordenar apenas imagens de usuário
+  final VoidCallback newImageFunction;
 
   const ImagesPicker({
     Key? key,
+    required this.newImageFunction,
     required this.imageFiles,
-  });
+    this.allowReorderingUserImagesOnly = false,
+  }) : super(key: key);
 
   @override
   _ImagesPickerState createState() => _ImagesPickerState();
 }
 
 class _ImagesPickerState extends State<ImagesPicker> {
+  final int maxImages = 6;
+
   @override
   void initState() {
     super.initState();
   }
 
+  // Função para substituir a imagem do usuário por uma imagem padrão
+  void _removeUserImage(int index) {
+    setState(() {
+      widget.imageFiles[index] = XFile("assets/emptyPhoto.png");
+      _reorderImages();  // Reordena após remover uma imagem do usuário
+    });
+  }
+
+  // Função para ordenar a lista de imagens
+  void _reorderImages() {
+    setState(() {
+      widget.imageFiles.sort((a, b) {
+        // Coloca as imagens padrão ("emptyPhoto.png") no final da lista
+        if (a.name == 'emptyPhoto.png' && b.name != 'emptyPhoto.png') {
+          return 1;  // a vai para o final
+        } else if (a.name != 'emptyPhoto.png' && b.name == 'emptyPhoto.png') {
+          return -1; // b vai para o final
+        } else {
+          return 0;  // mantêm a ordem
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Garante que sempre tenha 6 imagens na lista
+    while (widget.imageFiles.length < maxImages) {
+      widget.imageFiles.add(XFile("assets/emptyPhoto.png"));
+    }
+
     return ReorderableGridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3, crossAxisSpacing: 5, mainAxisSpacing: 5),
-        itemCount: widget.imageFiles.length,
-        shrinkWrap: true,
-        onReorder: ((oldIndex, newIndex) {
-          XFile path = widget.imageFiles.removeAt(oldIndex);
-          widget.imageFiles.insert(newIndex, path);
-          setState(() {});
-        }),
-        itemBuilder: (context, index) {
-          return Container(
-            key: ValueKey(index),
-            child: Image.file(File(widget.imageFiles![index].path)),
-          );
-        });
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 5,
+        mainAxisSpacing: 5,
+      ),
+      itemCount: widget.imageFiles.length,
+      shrinkWrap: true,
+      onReorder: (oldIndex, newIndex) {
+        // Verifica se a imagem arrastada ou a de destino é uma imagem padrão e evita reordenar
+        if (widget.imageFiles[oldIndex].name == 'emptyPhoto.png' ||
+            widget.imageFiles[newIndex].name == 'emptyPhoto.png') {
+
+          //Adicionar uma mensagem informando que o usuário não pode reordernar
+          return;  // Impede a reordenação se uma das imagens for uma imagem padrão
+        }
+
+        // Continua com a reordenação se for uma imagem do usuário
+        XFile path = widget.imageFiles.removeAt(oldIndex);
+        widget.imageFiles.insert(newIndex, path);
+        setState(() {});
+      },
+      itemBuilder: (context, index) {
+        return Stack(
+          key: ValueKey(index),
+          children: [
+            // Se for uma imagem padrão, exibe a imagem padrão e adiciona o GestureDetector
+            widget.imageFiles[index].name == 'emptyPhoto.png'
+                ? GestureDetector(
+                    onTap: () {
+                      widget.newImageFunction();  // Chama a função para adicionar nova imagem
+                    },
+                    child: Image.asset("assets/emptyPhoto.png", fit: BoxFit.cover),
+                  )
+                : Image.file(File(widget.imageFiles[index].path),
+                    fit: BoxFit.cover),
+            // Exibe o botão de "X" apenas nas imagens enviadas pelo usuário
+            if (widget.imageFiles[index].name != 'emptyPhoto.png')
+              Positioned(
+                right: 0,
+                bottom: 0,
+                child: IconButton(
+                  icon: const Icon(Icons.close, color: Colors.red),
+                  onPressed: () {
+                    _removeUserImage(index); // Remove a imagem e insere uma padrão
+                  },
+                ),
+              ),
+          ],
+        );
+      },
+    );
   }
 }
